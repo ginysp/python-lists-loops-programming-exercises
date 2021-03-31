6ea0d8618626ec231534d56b6c2e66834ade74bd
Gineth <ginysp@hotmail.com>
2021-03-31 02:56:37 +0000

    9inc

--- exercises/01.3-Print-the-last-one/app.py
+++ exercises/01.3-Print-the-last-one/app.py
@@ -10,5 +10,7 @@ def generate_random_list():
         i += i
     return aux_list
 my_stupid_list = generate_random_list()
+the_last_one = my_stupid_list[(len(my_stupid_list))-1]
 
 # Feel happy to write the code below this comment, good luck!:
+print(the_last_one)
